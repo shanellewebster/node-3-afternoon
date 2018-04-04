e7588f15195c762b3ac74fca120beb54c18cfff3
shanellewebster <webstershanelle@gmail.com>
2018-04-04 12:13:11 -0500

    fixed

--- server/index.js
+++ server/index.js
@@ -18,17 +18,21 @@ const search_controller = require('./controllers/search_controller');
 const app = express();
 //add bodyParser so we can read JSON from the request body
 app.use( bodyParser.json());
-// add session so we can create sessions. session needs a configuration object as the first argument. The object should have a secret, resave, and saveUninitialized property. secret can be any string you like that is stored in your .env file and resave and saveUninitialized should equal true.
+// add session so we can create sessions. session needs a configuration object as the first argument. The object should have a secret, resave, and saveUninitialized property. Secret is required. The rest are not. Secret can be any string you like that is stored in your .env file and resave and saveUninitialized should equal true.
 app.use(session({
     secret: process.env.SESSION_SECRET,
+    //decides whether to save a session that was not changed.
     resave: false,
+    //if you want to track.
     saveUninitialized: true
+    // COULD ADD MAXAGE HERE:
 }));
-//adding checkForSession
+
+//adding checkForSession BELOW 
 app.use(checkForSession);
 
 // to serve up the build folder in /build.
-app.use( express.static( `${__dirname}/build` ) );
+app.use( express.static( `${__dirname}/../build` ) );
 
 
 //make a GET endpoint at /api/swagthat calls the read method on our swag_controller.
